72b04f0411bda84422e62afb04eafafbe127b55d
sahidDev09 <iamsahidofficial99@gmail.com>
2026-01-06 13:27:38 +0600

    style: Adjust responsive text sizing for SectionHeader title and description.

--- components/Footer/SectionHeader.tsx
+++ components/Footer/SectionHeader.tsx
@@ -9,10 +9,10 @@ export const SectionHeader = ({
 }) => {
     return (
         <div className="flex flex-col gap-5 px-6 md:px-0"> 
-            <h1 className='md:text-6xl text-3xl font-heading tracking-wide'>
+            <h1 className='md:text-4xl lg:text-5xl text-3xl font-heading tracking-wide'>
                 <Highlighter action="underline">{title}</Highlighter>
             </h1>
-            <p className="text-sm font-display text-white/60">{description}</p>
+            <p className="text-sm font-display md:text-lg text-white/60">{description}</p>
             <hr className="border-t-2 border-gray-500 w-[calc(100%-5rem)] my-3 sm:w-auto" />
         </div>
     );
